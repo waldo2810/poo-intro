4cd785b7029090ae28cc57f79b5c6b9931c37bdf
Darkknight-spec <ismael.trocha@hotmail.com>
2023-03-09 10:54:00 -0500

    Utilizamos expresiones lambda en el main

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,32 @@ import PrimerPaquete.Carro;
 import PrimerPaquete.Moto;
 import PrimerPaquete.Vehiculo;
 
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        Bicicleta bici = new Bicicleta("RALY", "2023", "a", "montañera",20, 5);
-        Bicicleta bici1 = new Bicicleta("GW", "b", "b", "pista",20, 5);
-        Moto moto = new Moto("Ducatti", "2023","c" ,"1200", false);
-        Carro carro = new Carro("Mazda", "CX-60","d", "Blanco");
-        Carro carro1 = new Carro("Mazda", "CX-60","e", "Rojo");
+     List <Vehiculo> arrayVehiculos = new ArrayList();
+     Vehiculo carro = new Carro("Mazda","a-30","ABC-123","Rojo");
+     Vehiculo carro1 = new Carro("Ferrari","CX-40","DEF-456","Rojo");
+     Vehiculo carro2 = new Carro("Twingo","CX-50","GHI-789","Rojo");
+     Vehiculo carro3 = new Carro("Lamborghini","CX-60","JKL-321","Rojo");
+     Vehiculo carro4 = new Carro("Chevrolet","CX-70","MNO-654","Rojo");
+     Vehiculo carro5 = new Carro("BWM","CX-80","PQR-987","Rojo");
+     Vehiculo carro6 = new Carro("Jeep","CX-90","STU-102","Rojo");
 
+        arrayVehiculos.add(carro);
+        arrayVehiculos.add(carro1);
+        arrayVehiculos.add(carro2);
+        arrayVehiculos.add(carro3);
+        arrayVehiculos.add(carro4);
+        arrayVehiculos.add(carro5);
+        arrayVehiculos.add(carro6);
 
-
-        HashMap<Integer, Vehiculo> mapVehiculo = new HashMap<>();
-        HashSet <Vehiculo>hashVehiculo = new HashSet<>();
-        ArrayList<Vehiculo> arrayVehiculo= new ArrayList<>();
-        arrayVehiculo.add(bici);
-        arrayVehiculo.add(bici1);
-        arrayVehiculo.add(moto);
-        arrayVehiculo.add(carro1);
-        arrayVehiculo.add(carro);
-
-        Collections.sort(arrayVehiculo);
-        arrayVehiculo.forEach(System.out::println);
-        //System.out.println(carro1.compareTo(carro));
-
-
-
-
+    List <Vehiculo> listaVehiculos = arrayVehiculos.stream().filter(x -> x.getMarca().contains("a") && x.getModelo().contains("a")).collect(Collectors.toList());
+    listaVehiculos.forEach(System.out::println);
 
     }
-
 }
 
