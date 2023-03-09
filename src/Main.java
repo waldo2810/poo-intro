c2f844cc5cc3fd42f17ac44735df9786714c8771
Darkknight-spec <ismael.trocha@hotmail.com>
2023-03-09 12:28:25 -0500

    Utilizamos map para filtrar marca y la motos que estan asocidada a esa marca.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import PrimerPaquete.Bicicleta;
-import PrimerPaquete.Carro;
 import PrimerPaquete.Moto;
 import PrimerPaquete.Vehiculo;
 
@@ -12,12 +10,12 @@ public class Main {
      List <Moto> arrayVehiculos = new ArrayList();
 
         Moto moto= new Moto("Suzuki","A","JDK-12D",220,false);
-        Moto moto1= new Moto("Honda","A","JDK-12D",100,false);
-        Moto moto2= new Moto("Ducatti","A","JDK-12D",53,false);
+        Moto moto1= new Moto("Suzuki","A","JDK-12D",100,false);
+        Moto moto2= new Moto("Suzuki","A","JDK-12D",53,false);
         Moto moto3= new Moto("Boxer","A","JDK-12D",500,false);
-        Moto moto4= new Moto("N-MAX","A","JDK-12D",350,false);
-        Moto moto5= new Moto("AKT","A","JDK-12D",106,false);
-        Moto moto6= new Moto("PULSAR","A","JDK-12D",122,false);
+        Moto moto4= new Moto("Boxer","A","JDK-12D",350,false);
+        Moto moto5= new Moto("Boxer","A","JDK-12D",106,false);
+        Moto moto6= new Moto("Boxer","A","JDK-12D",122,false);
 
         arrayVehiculos.add(moto);
         arrayVehiculos.add(moto1);
@@ -27,7 +25,8 @@ public class Main {
         arrayVehiculos.add(moto5);
         arrayVehiculos.add(moto6);
 
-        filtrarList(arrayVehiculos).forEach(System.out::println);
+      //  showInformation(mapa(arrayVehiculos));
+        showMaxCilindraje(filtrarList(arrayVehiculos));
 
     }
 
@@ -37,12 +36,19 @@ public class Main {
     * */
 
 
-    public static List <Vehiculo>filtrarList(List<Moto>s){
-        return s.stream().max((a1,a2) -> a1.getCilindraje() - a2.getCilindraje()).stream().collect(Collectors.toList());
+    public static List <Moto>filtrarList(List<Moto>s){
+        return s.stream().max(Comparator.comparingInt(Moto::getCilindraje)).stream().collect(Collectors.toList());
+    }
+    public static Map<String, List<Moto>> mapa(List<Moto>motos){
+    return  motos.stream().collect(Collectors.groupingBy(Vehiculo::getMarca));
     }
 
-    public static void showInformation(List<?>filtrado ){
-        filtrado.forEach(System.out::println);
+    public static void showMaxCilindraje(List<Moto>motos){
+        motos.forEach(System.out::println);
     }
+    public static void showInformation(Map<String,List <Moto>>filtrado ){
+        filtrado.forEach((clave,valor)-> System.out.println("marca: "+clave+"Motos: "+valor));
+    }
+
 }
 
